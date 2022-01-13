b41892f94e0b5b8461ab5040c9927d47e0b45568
naruniyashan <naruniya5@gmail.com>
2022-01-13 21:35:36 +0800

    20220113 javascript 語法，控制按鈕

--- js/Java_Script.js
+++ js/Java_Script.js
@@ -26,6 +26,18 @@ function AddArticles() {
 
 // jQuery 語法
 // 行動裝置選單按鈕，開啟與關閉
+// 1. 隱藏 MobileNav 區塊
+$('#MobileNav').hide();
+
+// 2. 點擊按鈕 Menu 開啟 MobileNav 區塊
+$('#Menu').click( function() {
+    $('#MobileNav').show();
+});
+
+// 3. 點擊 Close 關閉 MobileNav 區塊
+$('.Close').click( function(){
+    $('#MobileNav').hide();
+});
 
 function AddArticle() {
     // 呼叫主要的 article id=main
