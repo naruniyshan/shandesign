dab48ce393c6168d6a3fb1cc9e9987392f577d71
naruniyashan <naruniya5@gmail.com>
2022-01-25 21:04:39 +0800

    20220125 javascript語法

--- js/Java_Script.js
+++ js/Java_Script.js
@@ -14,7 +14,7 @@ function AddArticle() {
     let Main = document.getElementById("main");
 
     // 產出 3 個標籤
-    for ( let i = 0; i < ArtId.length; i++ ) {
+    for (let i = 0; i < ArtId.length; i++) {
         // 要新增的標籤 article
         let art = document.createElement("article");
         let h1 = document.createElement("h1");
@@ -23,7 +23,7 @@ function AddArticle() {
         // 新增 article 的 id
         art.setAttribute("id", ArtId[i]);
         h1.setAttribute("id", ArtH1[i]);
-        div.setAttribute("class", ArtDiv[i]); 
+        div.setAttribute("class", ArtDiv[i]);
 
         // 在框架內新增子元素 main > article
         Main.appendChild(art);
@@ -40,6 +40,7 @@ function AddArticle() {
 
     // 將網頁資料，用載入的方式置入框架中
     $('.PorCon').load('../webs/graphic.html');
+    $('.AboCon').load('../webs/about.html');
 };
 
 // jQuery 語法
@@ -48,12 +49,12 @@ function AddArticle() {
 $('#MobileNav').hide();
 
 // 2. 點擊按鈕 Menu 開啟 MobileNav 區塊
-$('#Menu').click( function() {
+$('#Menu').click(function () {
     $('#MobileNav').show();
 });
 
 // 3. 點擊 Close 關閉 MobileNav 區塊
-$('.Close').click( function(){
+$('.Close').click(function () {
     $('#MobileNav').hide();
 });
 
@@ -67,14 +68,14 @@ function TopBtn() {
     Top.hide();
 
     // 設定按鈕行為
-    Topa.click( function() {
+    Topa.click(function () {
         // 執行的動畫：當點擊按鈕時，畫面捲動到網頁最上方，加上滑動的秒數
-        $('html, body').animate({scrollTop:0}, 0.333);
+        $('html, body').animate({ scrollTop: 0 }, 0.333);
     });
 
     // 畫面高度 大於 300px，按鈕會出現；低於 300px，按鈕會隱藏
     $(window).scroll(function () {
-        if( $(this).scrollTop() > 300 ) {
+        if ($(this).scrollTop() > 300) {
             // 淡入
             Top.fadeIn(222);
         } else {
@@ -95,16 +96,14 @@ function BannerChang() {
     let Images = document.getElementById('Images');
     let AllButton = $('#AllButtons > label');
     let AllLeng = AllButton.length;
-    let BannerBox = document.getElementById('BannerBox');
 
     AllButtons();
-    AutoChang();
     function AllButtons() {
-        
+
         // 迴圈 新增按鈕
-        for ( let i = 0; i < AllLeng; i++ ) {
+        for (let i = 0; i < AllLeng; i++) {
             // 按鈕執行
-            $(AllButton[i]).click( function(ev) {
+            $(AllButton[i]).click(function (ev) {
                 // ev.currentTarget 監聽群組物件
                 // index() 獲取 label 的索引
                 // 移動的距離 npx = 索引 * 圖片的寬度
@@ -115,42 +114,48 @@ function BannerChang() {
                     transform: 'translateX(' + npx + 'vw)',
                     transition: 'transform 1s'
                 });
+
+                // 將 label 索引值給予 n
+                n = index;
+                AllButton.eq(n);
             });
         }
     }
 
+    // 自動輪播
+
+    // 宣告計時器變數 找到要切換的第 n 個 button
+    let n = 0;
+
+    // 重覆執行一個函數或是一段代碼，每次調用之間具有固定時間延遲 setInterval() 
+    // => 箭頭函式
+    let Timer = setInterval(() => {
+        n++;
+        // eq() 遍歷方法 - 返回帶有被選元素的指定索引號的元素 找出第 n %(餘數) AllLeng
+        // trigger( 事件類型 )觸發被選元素的指定事件類型 例如： click submit（送出）
+        // addClass( 類別class名稱 )元素添加一個或多個類別
+        // siblings( 類別或ID 名稱)匹配集合中每個元素的同胞
+        // removeClass( 類別class名稱 )移除 class 名稱
+        AllButton.eq(n % AllLeng).trigger('click').addClass('orange').siblings('.orange').removeClass('orange');
+    }, 3000);
+
     // 滑鼠移入圖片，清除計時器（停止）
-    // $(BannerBox).on('mouseenter',function() {
-    //     BannerBox.clearInterval( AutoChang() ) ;
-    // });
+    $('#BannerBox').on('mouseenter', function () {
+        window.clearInterval(Timer);
+    });
 
     // 滑鼠移出圖片，恢復計時
-    // $(BannerBox).mouseleave(function() {
-    //     AutoChang();
-    // });
-
-    // 自動輪播
-    function AutoChang() {
-        // 宣告計時器變數 找到要切換的第 n 個 button
-        let n = 0;
-
-        // 重覆執行一個函數或是一段代碼，每次調用之間具有固定時間延遲 setInterval() 
-        // => 箭頭函式
-        setInterval( ( ) => {
+    $('#BannerBox').on('mouseleave', function () {
+        Timer = setInterval(() => {
             n++;
-            // eq() 遍歷方法 - 返回帶有被選元素的指定索引號的元素 找出第 n %(餘數) AllLeng
-            // trigger( 事件類型 )觸發被選元素的指定事件類型 例如： click submit（送出）
-            // addClass( 類別class名稱 )元素添加一個或多個類別
-            // siblings( 類別或ID 名稱)匹配集合中每個元素的同胞
-            // removeClass( 類別class名稱 )移除 class 名稱
-            AllButton.eq(n%AllLeng).trigger('click').addClass('blue').siblings('.blue').removeClass('blue');
-        }, 3000 );
-    }
+            AllButton.eq(n % AllLeng).trigger('click').addClass('blue').siblings('.blue').removeClass('blue');
+        }, 3000);
+    });
 
 
     function Buttons() {
-            // 按鈕執行的動作
-        $(P01).click(function(){
+        // 按鈕執行的動作
+        $(P01).click(function () {
             // jQuery 函式庫語法
             // $(Images).css({
             //     transform: 'translateX(0vw)',
@@ -162,28 +167,28 @@ function BannerChang() {
             Images.style.transtiton = 'transform 1s';
         });
 
-        $(P02).click(function() {
+        $(P02).click(function () {
             $(Images).css({
                 transform: 'translateX(-100vw)',
                 transition: 'transform 1s'
             })
         });
 
-        $(P03).click(function() {
+        $(P03).click(function () {
             $(Images).css({
                 transform: 'translateX(-200vw)',
                 transition: 'transform 1s'
             })
         });
 
-        $(P04).click(function() {
+        $(P04).click(function () {
             $(Images).css({
                 transform: 'translateX(-300vw)',
                 transition: 'transform 1s'
             })
         });
 
-        $(P05).click(function() {
+        $(P05).click(function () {
             $(Images).css({
                 transform: 'translateX(-400vw)',
                 transition: 'transform 1s'
